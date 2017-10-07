b31c7fe22dbe9dc7bc7996f1d4c4281e45553319
Wan <yuxi.wan@gmail.com>
2017-10-07 21:55:43 +0800

    add delete operation

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// const {
+//     MongoClient,
+//     ObjectID
+// } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+
+    console.log('Connected to MongoDB server.')
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text: 'Something todo'}).then((data) => {
+    //     console.log(data.result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Something todo'}).then((data) => {
+    //     console.log(data.result);
+    // });
+
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({text: 'Something todo'}).then((data) => {
+        console.log(data);
+    });
+
+    
+    db.close();
+
+})
