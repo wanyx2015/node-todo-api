7f001d0a6cc62f731b738978127229eae25f7ab0
Wan <yuxi.wan@gmail.com>
2017-10-19 12:37:29 +0800

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var ObjectID = require('mongodb').ObjectID;
-var mongoose = require('./db/mongoose').mongoose;
-var Todo = require('./models/todo').Todo;
-var User = require('./models/user').User;
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
+const mongoose = require('./db/mongoose').mongoose;
+const Todo = require('./models/todo').Todo;
+const User = require('./models/user').User;
 
 const port = process.env.PORT || 3000;
 
@@ -30,8 +31,6 @@ app.post('/todos', (req, res) => {
 
 // GET /todos
 app.get('/todos', (req, res) => {
-    console.log(req.body);
-
     Todo.find().then((docs) => {
         res.send({
             docs,
@@ -54,7 +53,6 @@ app.get('/todos/:id', (req, res) => {
 
     // Catch error instead of error handler
     Todo.findById(id).then((doc) => {
-        console.log(doc);
         if (!doc) {
             return res.status(404).send("No document found!");
         }
@@ -70,13 +68,9 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send(e);
 })
 
-
-
-// GET /todo/12345
+// DELETE /todo/12345
 app.delete('/todos/:id', (req, res) => {
-
     var id = req.params.id;
-
     if (!ObjectID.isValid(id)) {
         console.log(`Invalid object id: ${id}`);
         return res.status(404).send(`Invalid object id: ${id}`);
@@ -84,7 +78,6 @@ app.delete('/todos/:id', (req, res) => {
 
     // Catch error instead of error handler
     Todo.findByIdAndRemove(id).then((doc) => {
-        console.log(doc);
         if (!doc) {
             return res.status(404).send("No document found!");
         }
@@ -100,6 +93,52 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send(e);
 })
 
+// PATCH /todo/12345
+app.patch('/todos/:id', (req, res) => {
+
+    console.log('PATCH request body:', req.body);
+    console.log('PATCH request params', req.params);
+
+    var id = req.params.id;
+    // accepted fields that user can change
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        console.log(`Invalid object id: ${id}`);
+        return res.status(404).send(`Invalid object id: ${id}`);
+    }
+
+
+
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completedAt = null;
+        body.completed = false;
+    }
+
+    // Catch error instead of error handler
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((doc) => {
+        if (!doc) {
+            return res.status(404).send("No document found!");
+        }
+
+        res.send({
+            doc,
+            status: true
+        });
+    }).catch((e) => {
+        res.status(404).send(e);
+    })
+
+}, (e) => {
+    res.status(404).send(e);
+})
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
