cba9c903be2daf880401fe399ed015e676804ef6
Wan <yuxi.wan@gmail.com>
2017-10-19 08:46:33 +0800

    add mongoose

--- server/server.js
+++ server/server.js
@@ -1,38 +1,51 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) =>{
+    console.log(req.body);
+    
+    var todo = new Todo(req.body);
+    
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 })
 
-var newTodo = Todo({
-    text: "Cook dinner"
-})
+// var newTodo = Todo({
+//     text: "Cook dinner"
+// })
 
-newTodo.save().then((doc) => {
-    console.log(doc);
-}, (err) => {
-    console.log("Unable to create doc.")
-})
+// newTodo.save().then((doc) => {
+//     console.log(doc);
+// }, (err) => {
+//     console.log("Unable to create doc.")
+// })
 
-var otherTodo = Todo({
-    text: "Fix the laptop",
-    completed: true,
-    completedAt: 123,
-    // below field won't save to db
-    newF: 111
-}).save().then((doc) => {
-    console.log(doc);
-}, (err) => {
-    console.log("Unable to create doc.")
-})
+// var otherTodo = Todo({
+//     text: "Fix the laptop",
+//     completed: true,
+//     completedAt: 123,
+//     // below field won't save to db
+//     newF: 111
+// }).save().then((doc) => {
+//     console.log(doc);
+// }, (err) => {
+//     console.log("Unable to create doc.")
+// })
